bc7b487778eaceb773358955eda6507d6bd45a5e
이준규 <jkjkjk5710@gmail.com>
2022-01-08 19:19:47 +0900

    init spring project

---
+++ src/main/java/isntkyu/isntkyuspring/IsntkyuSpringApplication.java
@@ -0,0 +1,13 @@
+package isntkyu.isntkyuspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsntkyuSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsntkyuSpringApplication.class, args);
+	}
+
+}
