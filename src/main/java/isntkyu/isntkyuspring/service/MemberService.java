046d99d16bc5c2c40908b6adf8bf5293516b3abe
이준규 <jkjkjk5710@gmail.com>
2022-01-09 16:51:01 +0900

    domain, service , Test code 개발

---
+++ src/main/java/isntkyu/isntkyuspring/service/MemberService.java
@@ -0,0 +1,39 @@
+package isntkyu.isntkyuspring.service;
+
+import isntkyu.isntkyuspring.domain.Member;
+import isntkyu.isntkyuspring.repository.MemberRepository;
+import isntkyu.isntkyuspring.repository.MemoryMemberRepository;
+import org.springframework.expression.spel.ast.OpInc;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    public final MemberRepository memberRepository;
+
+    public MemberService (MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member) {
+        validateDublicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDublicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne (Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
