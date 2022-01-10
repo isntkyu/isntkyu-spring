880e632ace801f50b0b4a58ff73355d7858a257d
이준규 <jkjkjk5710@gmail.com>
2022-01-10 22:26:58 +0900

    domain, service , Test code 개발

--- src/test/java/isntkyu/isntkyuspring/service/MemberServiceTest.java
+++ src/test/java/isntkyu/isntkyuspring/service/MemberServiceTest.java
@@ -54,7 +54,7 @@ class MemberServiceTest {
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
 
         //then
-        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
     }
 
     @Test
