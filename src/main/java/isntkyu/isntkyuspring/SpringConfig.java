306415a72c81b53e5332dc03ad1de3c5804b98aa
이준규 <jkjkjk5710@gmail.com>
2022-01-16 17:51:29 +0900

    스프링 데이터 jpa ;;

--- src/main/java/isntkyu/isntkyuspring/SpringConfig.java
+++ src/main/java/isntkyu/isntkyuspring/SpringConfig.java
@@ -2,34 +2,38 @@ package isntkyu.isntkyuspring;
 
 import isntkyu.isntkyuspring.repository.JpaMemberRepository;
 import isntkyu.isntkyuspring.repository.MemberRepository;
-import isntkyu.isntkyuspring.repository.MemoryMemberRepository;
 import isntkyu.isntkyuspring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
 
 //    private final DataSource dataSource;
-    private final EntityManager em;
+//    private final EntityManager em;
+
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+////        this.dataSource = dataSource;
+//        this.em = em;
+//    }
+
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-//        this.dataSource = dataSource;
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new JpaMemberRepository(em);
+//    }
 }
